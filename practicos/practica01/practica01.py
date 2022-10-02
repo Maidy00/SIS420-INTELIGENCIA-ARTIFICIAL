0e43b160da51b4aa09b5429f974171952d08d4f1
Maidy Rocio Mamani Lugo <81988724+Maidy00@users.noreply.github.com>
2022-10-02 16:00:35 -0400

    Add files via upload

---
+++ practicos/practica01/practica01.py
@@ -0,0 +1,23 @@
+while True:
+    print("BIOGRAFIA DE MAIDY")
+    print("1. Nombre")
+    print("2.edad")
+    print("3. carrera")
+    print("4.toda la biografia")
+    #3print("5.salir")
+
+    biografia = int(input())
+    if biografia == 1:
+        print("Nombre: Maidy Rocio Mamani Lugo")
+    elif biografia ==2:
+        print("Edad: 22 años")
+    elif biografia == 3:
+        print("Carrera: ING. Diseño y Animacion Digital")
+    elif biografia == 4:
+        print("NOMBRE: Maidy Rocio Mamani Lugo \n" 
+    "EDAD: 22 años \n"  
+    "CARRERA: Ing. diseño y animacion digital  en la USFX \n"
+    "ESCUELA: Fray geraldo Maldini - Potosi \n"
+    "COLEGIO: Liceo de Señoritas Manuel Rodriguez de Quiroga \n")
+    else:
+        print("Esa opcion no existe, POR FAVOR DIGITE ENTRE 1-4")
